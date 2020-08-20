797d66aff347a54155a995617fec1b9ba7fd6d18
Antonio Peralta <1peraltaadonis@gmail.com>
2020-08-19 21:23:13 -0400

    Add files via upload

---
+++ smarttravel/js/main.js
@@ -0,0 +1,24 @@
+
+
+// loader
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.visibility = "visible";
+}
+
+// for mobile menu bar
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
